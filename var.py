61ec101df688d9a9b5f17d90b20efb6bfdc4aea9
rootuttu <98271251+rootuttu@users.noreply.github.com>
2022-03-13 23:19:58 +0530

    Update var.py

--- var.py
+++ var.py
@@ -5,7 +5,7 @@ class Var(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     # 6 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    legend_string = os.environ.get("eviral_STRING", None)
+    STRING = os.environ.get("eviral_STRING", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
     TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", None)
     LOGGER = True
